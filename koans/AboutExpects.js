13ac2b7c5b9a8862c2c5f6fa001f455ddce3d521
mrdavidlaing <david@davidlaing.com>
2010-12-13 19:47:18 +0000

    Re-add koans/AboutAsserts.js - thanks hakanson!

---
+++ koans/AboutExpects.js
@@ -0,0 +1,38 @@
+describe("About Expects", function() {
+
+  //We shall contemplate truth by testing reality, via spec expectations.  
+  it("should expect true", function() {
+    expect(false).toBeTruthy(); //This should be true
+  });
+
+  //To understand reality, we must compare our expectations against reality.
+  it("should expect equality", function () { 
+	  var expectedValue = __;
+	  var actualValue = 1 + 1;
+	
+	  expect(expectedValue === actualValue).toBeTruthy();
+  });  
+
+  //Some ways of asserting equality are better than others.
+  it("should assert equality a better way", function () { 
+	  var expectedValue = __;
+	  var actualValue = 1 + 1;
+	
+    // toEqual() compares using common sense equality
+	  expect(actualValue).toEqual(expectedValue);
+  });
+
+  //Sometime you need to be really exact
+  it("should assert equality with ===", function () { 
+	  var expectedValue = __;
+	  var actualValue = 1 + 1;
+	
+    // toBe() will always use === to compare
+	  expect(actualValue).toBe(expectedValue);
+  });  
+
+  //Sometimes we will ask you to fill in the values
+  it("should have filled in values", function () {
+	  expect(__).toEqual(1 + 1);
+  });
+});
