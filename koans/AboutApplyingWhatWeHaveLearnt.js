71ecd8b2224ec6ceb40cd643e9a6a75972b664c0
Mathieu Agopian <mathieu.agopian@gmail.com>
2013-02-21 08:32:37 +0100

    Update koans/AboutApplyingWhatWeHaveLearnt.js
    Here, it's the code we have to find, so I believe the answer should be provided

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -83,7 +83,7 @@ describe("About Applying What We Have Learnt", function() {
 
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
