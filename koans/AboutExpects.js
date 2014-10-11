f9e3bc41d1df27d2a37e714bff6590b6f84ed23e
Hamza <ismnoiet@users.noreply.github.com>
2014-10-11 23:13:19 +0100

    Update AboutExpects.js
    add truthy and falsy examples

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -4,6 +4,33 @@ describe("About Expects", function() {
   it("should expect true", function() {
     expect(false).toBeTruthy(); //This should be true
   });
+  
+  // we expect to be truthy
+  it("should by truthy",function(){
+    expect(true).toBeFalsy();
+    expect(1).toBeFalsy(); // any number >0 will be accepted
+    expect("s").toBeFalsy(); // any string different than "" will be accepted
+  });
+  
+  // we expect to be falsy
+  it("should be falsy",function(){
+    var variable;
+    var functionWithNoReturn = function(){
+    }
+    expect(false).toBeTruthy();
+    expect(null).toBeTruthy();
+    expect(undefined).toBeTruthy();  
+    expect(variable).toBeTruthy();  // declared variable without a value returns undefined value
+    expect(functionWithNoReturn()).toBeTruthy(); // function without return vlaue returns undefined value
+    
+    expect(0).toBeTruthy(); // any number < = 0 will be accepted 
+    expect("").toBeTruthy();
+    expect(NaN).toBeTruthy();
+    
+    
+  });
+  
+  
 
   // To understand reality, we must compare our expectations against reality.
   it("should expect equality", function () {
