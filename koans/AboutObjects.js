282b96ce759ad8fd24d7ce10427e5abdf8f2bb17
Richard Littauer <richard.littauer@gmail.com>
2014-11-23 01:39:02 -0800

    Added another describe
    Before, when running the KoanRunner, the 'in keyword' describe would appear
    before the two expects above it if it hadn't been filled out yet, which isn't
    the expected functionality. By adding another describe block, this issue goes
    away and we also have more description of those two tests.
    
    It might be a good idea to add a fourth describe block to the final two tests
    on this page, just to be consistent. Those would be adding and deltion, as well
    as prototypes. Just an idea.

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -17,35 +17,36 @@ describe("About Objects", function () {
     });
   });
 
+  describe("Methods", function() {
+    it("should know properties that are functions act like methods", function () {
+      var megalomaniac = {
+        mastermind : "Brain",
+        henchman: "Pinky",
+        battleCry: function (noOfBrains) {
+          return "They are " + this.henchman + " and the" +
+            Array(noOfBrains + 1).join(" " + this.mastermind);
+        }
+      };
 
-  it("should know properties that are functions act like methods", function () {
-    var megalomaniac = {
-      mastermind : "Brain",
-      henchman: "Pinky",
-      battleCry: function (noOfBrains) {
-        return "They are " + this.henchman + " and the" +
-          Array(noOfBrains + 1).join(" " + this.mastermind);
-      }
-    };
+      var battleCry = megalomaniac.battleCry(4);
+      expect(FILL_ME_IN).toMatch(battleCry);
+    });
 
-    var battleCry = megalomaniac.battleCry(4);
-    expect(FILL_ME_IN).toMatch(battleCry);
-  });
+    it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
+      var currentDate = new Date();
+      var currentYear = (currentDate.getFullYear());
+      var megalomaniac = {
+        mastermind: "James Wood",
+        henchman: "Adam West",
+        birthYear: 1970,
+        calculateAge: function () {
+          return currentYear - this.birthYear;
+        }
+      };
 
-  it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
-    var currentDate = new Date();
-    var currentYear = (currentDate.getFullYear());
-    var megalomaniac = {
-      mastermind: "James Wood",
-      henchman: "Adam West",
-      birthYear: 1970,
-      calculateAge: function () {
-        return currentYear - this.birthYear;
-      }
-    };
-
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+      expect(currentYear).toBe(FILL_ME_IN);
+      expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    });
   });
 
   describe("'in' keyword", function () {
