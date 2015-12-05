8f497decbc014e8859b9b21340caa37f894e37f4
Adam Smith <adamsmith910@aol.com>
2015-12-05 10:35:13 -0500

    added higherorderfunctions, inheritance, mutability and objects koans.  Started on applyingwhatwehavelearnt.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,14 +32,14 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
       var productsICanEat = [];
 
-      /* solve using filter() & all() / any() */
+      
 
       expect(productsICanEat.length).toBe(FILL_ME_IN);
   });
