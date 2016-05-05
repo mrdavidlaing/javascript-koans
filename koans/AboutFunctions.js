d0c6a26de8d0c56b85d2e5232d572e3c70f8cf56
Mariah Tree <mariah.tree@gmail.com>
2016-05-05 15:23:27 -0500

    14/53

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -21,9 +21,9 @@ describe("About Functions", function() {
       return message;
     }
 
-    expect(getMessage()).toBe(FILL_ME_IN);
-    expect(overrideMessage()).toBe(FILL_ME_IN);
-    expect(message).toBe(FILL_ME_IN);
+    expect(getMessage()).toBe("Outer");
+    expect(overrideMessage()).toBe("Inner");
+    expect(message).toBe("Outer");
   });
 
   it("should have lexical scoping", function () {
