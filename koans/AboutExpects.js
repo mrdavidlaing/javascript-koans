22bff2b54bd2bd0b0475aeb950da959ab2b59ace
Mariah Tree <mariah.tree@gmail.com>
2016-05-05 14:35:32 -0500

    2/53

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -9,7 +9,7 @@ describe('About Expects', function() {
 
   // To understand reality, we must compare our expectations against reality.
   it('should expect equality', function() {
-    var expectedValue = FILL_ME_IN;
+    var expectedValue = 2;
     var actualValue = 1 + 1;
 
     expect(actualValue === expectedValue).toBeTruthy();
