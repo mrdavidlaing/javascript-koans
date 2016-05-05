fc75af188532eda05ddb6c990dadbe3fdb56b9ef
Mariah Tree <mariah.tree@gmail.com>
2016-05-05 16:15:41 -0500

    31/53

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -11,9 +11,9 @@ describe("About Higher Order Functions", function () {
     var numbers = [1,2,3];
     var odd = _(numbers).filter(function (x) { return x % 2 !== 0 });
 
-    expect(odd).toEqual(FILL_ME_IN);
-    expect(odd.length).toBe(FILL_ME_IN);
-    expect(numbers.length).toBe(FILL_ME_IN);
+    expect(odd).toEqual([1, 3]);
+    expect(odd.length).toBe(2);
+    expect(numbers.length).toBe(3);
   });
 
   it("should use 'map' to transform each element", function () {
@@ -87,4 +87,3 @@ describe("About Higher Order Functions", function () {
   });
 
 });
-
