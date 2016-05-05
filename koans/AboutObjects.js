657cc020109fde68c3f06af7b13073fa835f6879
Mariah Tree <mariah.tree@gmail.com>
2016-05-05 15:43:56 -0500

    22/53

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2016);
+    expect(megalomaniac.calculateAge()).toBe(46);
   });
 
   describe("'in' keyword", function () {
