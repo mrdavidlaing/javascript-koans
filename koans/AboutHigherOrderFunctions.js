0e299654249c921ad60e5eeeb459291b6dc02cde
Mariah Tree <mariah.tree@gmail.com>
2016-05-06 12:06:15 -0500

    39/53

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -83,7 +83,7 @@ describe("About Higher Order Functions", function () {
                        .reduce(function (sum, x) { return sum + x })
                        .value();
 
-      expect(result).toEqual(FILL_ME_IN);
+      expect(result).toEqual(6);
   });
 
 });
