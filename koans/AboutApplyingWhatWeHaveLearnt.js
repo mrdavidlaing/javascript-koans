8ebe16488bc35b38d363b1f1161a751070c4c479
Daria <daria.calwell@gmail.com>
2016-06-09 12:42:15 -0400

    Add modified AboutApplying and AboutInheritance files

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,21 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
       var productsICanEat = [];
+      var hasMushrooms = _(products).filter(function(x) {return x = !hasMushrooms});
+      var containsNuts = _(products).filter(function(x) {return x = !containsNuts});
+      productsICanEat.noMushroomsnoNuts = function(x) {return x =!hasMushrooms & !containsNuts};
+
+
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(0);
   });
 
   /*********************************************************************************/
@@ -55,14 +60,14 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
     var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(true);
   });
 
   /*********************************************************************************/
