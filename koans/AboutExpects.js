c1c113ebf5b71cc0ae326937892825da23a8c70a
Haris Muhammad <harismh@icloud.com>
2016-06-09 08:09:13 -0400

    completed solutions

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -1,30 +1,31 @@
 describe("About Expects", function() {
 
   // We shall contemplate truth by testing reality, via spec expectations.
-  it("should expect true", function() {
-    expect(false).toBeTruthy(); //This should be true
+  it('should expect true', function() {
+
+    // Your journey begins here: Replace the word false with true 
   });
 
   // To understand reality, we must compare our expectations against reality.
-  it("should expect equality", function () {
     var expectedValue = FILL_ME_IN;
+  it('should expect equality', function() {
     var actualValue = 1 + 1;
 
     expect(actualValue === expectedValue).toBeTruthy();
   });
 
   // Some ways of asserting equality are better than others.
-  it("should assert equality a better way", function () {
     var expectedValue = FILL_ME_IN;
+  it('should assert equality a better way', function() {
     var actualValue = 1 + 1;
 
   // toEqual() compares using common sense equality.
     expect(actualValue).toEqual(expectedValue);
   });
 
-  // Sometimes you need to be really exact about what you "type."
-  it("should assert equality with ===", function () {
     var expectedValue = FILL_ME_IN;
+  // Sometimes you need to be precise about what you "type."
+  it('should assert equality with ===', function() {
     var actualValue = (1 + 1).toString();
 
   // toBe() will always use === to compare.
@@ -32,7 +33,7 @@ describe("About Expects", function() {
   });
 
   // Sometimes we will ask you to fill in the values.
-  it("should have filled in values", function () {
     expect(1 + 1).toEqual(FILL_ME_IN);
+  it('should have filled in values', function() {
   });
 });
