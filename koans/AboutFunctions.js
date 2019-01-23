d05662bdbd985a9e52a6087b671244a9295f0a45
Bridget Williams <bridget_williams1@unc.edu>
2019-01-23 14:08:51 -0500

    Finished About Arrays and asnwered first about functions problem

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -6,7 +6,7 @@ describe("About Functions", function() {
       return a + b;
     }
 
-    expect(add(1, 2)).toBe(FILL_ME_IN);
+    expect(add(1, 2)).toBe(3);
   });
 
   it("should know internal variables override outer variables", function () {
