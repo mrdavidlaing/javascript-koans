2242eca7395ca8f26139f56a06d1a655bbf5d0a6
Ben Thornton <smenjas@gmail.com>
2020-12-05 12:39:11 -0800

    Update old Doug Crockford URLs.

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -3,7 +3,7 @@ describe("About Arrays", function() {
   //We shall contemplate truth by testing reality, via spec expectations.
   it("should create arrays", function() {
     var emptyArray = [];
-    expect(typeof(emptyArray)).toBe(FILL_ME_IN); //A mistake? - http://javascript.crockford.com/remedial.html
+    expect(typeof(emptyArray)).toBe(FILL_ME_IN); //A mistake? - https://www.crockford.com/javascript/remedial.html
     expect(emptyArray.length).toBe(FILL_ME_IN);
 
     var multiTypeArray = [0, 1, "two", function () { return 3; }, {value1: 4, value2: 5}, [6, 7]];
