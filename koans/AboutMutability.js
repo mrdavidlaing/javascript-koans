5c6ac475d99668b64dfd902686d00a054c81c60f
Malkov <work.malkov.matvey@gmail.com>
2021-04-11 21:40:54 +0500

    add solutions

--- koans/AboutMutability.js
+++ koans/AboutMutability.js
@@ -1,27 +1,24 @@
-describe("About Mutability", function() {
-
+describe("About Mutability", function () {
   it("should expect object properties to be public and mutable", function () {
-    var aPerson = {firstname: "John", lastname: "Smith" };
+    var aPerson = { firstname: "John", lastname: "Smith" };
     aPerson.firstname = "Alan";
 
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should understand that constructed properties are public and mutable", function () {
-    function Person(firstname, lastname)
-    {
+    function Person(firstname, lastname) {
       this.firstname = firstname;
       this.lastname = lastname;
     }
-    var aPerson = new Person ("John", "Smith");
+    var aPerson = new Person("John", "Smith");
     aPerson.firstname = "Alan";
 
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should expect prototype properties to be public and mutable", function () {
-    function Person(firstname, lastname)
-    {
+    function Person(firstname, lastname) {
       this.firstname = firstname;
       this.lastname = lastname;
     }
@@ -29,40 +26,44 @@ describe("About Mutability", function() {
       return this.firstname + " " + this.lastname;
     };
 
-    var aPerson = new Person ("John", "Smith");
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    var aPerson = new Person("John", "Smith");
+    expect(aPerson.getFullName()).toBe("John Smith");
 
     aPerson.getFullName = function () {
       return this.lastname + ", " + this.firstname;
     };
 
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe("Smith, John");
   });
 
   it("should know that variables inside a constructor and constructor args are private", function () {
-    function Person(firstname, lastname)
-    {
+    function Person(firstname, lastname) {
       var fullName = firstname + " " + lastname;
 
-      this.getFirstName = function () { return firstname; };
-      this.getLastName = function () { return lastname; };
-      this.getFullName = function () { return fullName; };
+      this.getFirstName = function () {
+        return firstname;
+      };
+      this.getLastName = function () {
+        return lastname;
+      };
+      this.getFullName = function () {
+        return fullName;
+      };
     }
-    var aPerson = new Person ("John", "Smith");
+    var aPerson = new Person("John", "Smith");
 
     aPerson.firstname = "Penny";
     aPerson.lastname = "Andrews";
     aPerson.fullName = "Penny Andrews";
 
-    expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
-    expect(aPerson.getLastName()).toBe(FILL_ME_IN);
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFirstName()).toBe("John");
+    expect(aPerson.getLastName()).toBe("Smith");
+    expect(aPerson.getFullName()).toBe("John Smith");
 
     aPerson.getFullName = function () {
       return aPerson.lastname + ", " + aPerson.firstname;
     };
 
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe("Andrews, Penny");
   });
-
 });
