4c68d76d1394fec78ab618615b5a395050bc75c1
M.RAY <a.mray47@yahoo.fr>
2022-06-05 22:17:23 +0800

    karma about functions 4th question internal var override outer var

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -23,7 +23,7 @@ describe("About Functions", function() {
 
     expect(getMessage()).toBe('Outer');
     expect(overrideMessage()).toBe('Inner');
-    expect(message).toBe('Inner');
+    expect(message).toBe('Outer');
   });
 
   it("should have lexical scoping", function () {
