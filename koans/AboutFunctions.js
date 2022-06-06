bbdc09b67dfc846fbb2a880f354d3514cc4238ad
M.RAY <a.mray47@yahoo.fr>
2022-06-06 22:31:14 +0800

    karma about functions lexical scoping returning first argument

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -61,7 +61,7 @@ describe("About Functions", function() {
       return firstArg;
     }
 
-    expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
+    expect(returnFirstArg("first", "second", "third")).toBe('first');
 
     function returnSecondArg(firstArg, secondArg) {
       return secondArg;
