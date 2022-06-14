e12e2a829e2d34ce84803840b1eab73509d06c11
M.RAY <a.mray47@yahoo.fr>
2022-06-14 21:43:46 +0800

    About Objects, using prototype to add value to Object

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -103,7 +103,7 @@ describe("About Objects", function () {
         return "This circle has a radius of: " + this.radius;
       };
 
-      expect(simpleCircle.describe()).toBe(FILL_ME_IN);
+      expect(simpleCircle.describe()).toBe('this circle has a radius of: 10');
       expect(colouredCircle.describe()).toBe(FILL_ME_IN);
   });
 });
