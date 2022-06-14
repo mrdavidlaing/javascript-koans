d77e363369c7d1099acc157786cf19a1e011ca68
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-06-14 19:09:19 +0900

    AboutFunctions(완)

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -1,49 +1,44 @@
-describe("About Functions", function() {
-
-  it("should declare functions", function() {
-
+describe('About Functions', function () {
+  it('should declare functions', function () {
     function add(a, b) {
       return a + b;
     }
 
-    expect(add(1, 2)).toBe(FILL_ME_IN);
+    expect(add(1, 2)).toBe(3);
   });
 
-  it("should know internal variables override outer variables", function () {
-    var message = "Outer";
+  it('should know internal variables override outer variables', function () {
+    var message = 'Outer';
 
     function getMessage() {
       return message;
     }
 
     function overrideMessage() {
-      var message = "Inner";
+      var message = 'Inner';
       return message;
     }
 
-    expect(getMessage()).toBe(FILL_ME_IN);
-    expect(overrideMessage()).toBe(FILL_ME_IN);
-    expect(message).toBe(FILL_ME_IN);
+    expect(getMessage()).toBe('Outer');
+    expect(overrideMessage()).toBe('Inner');
+    expect(message).toBe('Outer');
   });
 
-  it("should have lexical scoping", function () {
-    var variable = "top-level";
+  it('should have lexical scoping', function () {
+    var variable = 'top-level';
     function parentfunction() {
-      var variable = "local";
+      var variable = 'local';
       function childfunction() {
         return variable;
       }
       return childfunction();
     }
-    expect(parentfunction()).toBe(FILL_ME_IN);
+    expect(parentfunction()).toBe('local');
   });
 
-  it("should use lexical scoping to synthesise functions", function () {
-
-    function makeMysteryFunction(makerValue)
-    {
-      var newFunction = function doMysteriousThing(param)
-      {
+  it('should use lexical scoping to synthesise functions', function () {
+    function makeMysteryFunction(makerValue) {
+      var newFunction = function doMysteriousThing(param) {
         return makerValue + param;
       };
       return newFunction;
@@ -52,49 +47,48 @@ describe("About Functions", function() {
     var mysteryFunction3 = makeMysteryFunction(3);
     var mysteryFunction5 = makeMysteryFunction(5);
 
-    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(FILL_ME_IN);
+    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(23);
   });
 
-  it("should allow extra function arguments", function () {
-
+  it('should allow extra function arguments', function () {
     function returnFirstArg(firstArg) {
       return firstArg;
     }
 
-    expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
+    expect(returnFirstArg('first', 'second', 'third')).toBe('first');
 
     function returnSecondArg(firstArg, secondArg) {
       return secondArg;
     }
 
-    expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
+    expect(returnSecondArg('only give first arg')).toBe(undefined);
 
     function returnAllArgs() {
       var argsArray = [];
       for (var i = 0; i < arguments.length; i += 1) {
         argsArray.push(arguments[i]);
       }
-      return argsArray.join(",");
-    }
+      return argsArray.join(',');
+    } // arguments를 통해 함수에 들어가는 인자들을 받아올 수 있음
 
-    expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
+    expect(returnAllArgs('first', 'second', 'third')).toBe(
+      'first,second,third'
+    );
   });
 
-  it("should pass functions as values", function () {
-
+  it('should pass functions as values', function () {
     var appendRules = function (name) {
-      return name + " rules!";
+      return name + ' rules!';
     };
 
     var appendDoubleRules = function (name) {
-      return name + " totally rules!";
+      return name + ' totally rules!';
     };
 
     var praiseSinger = { givePraise: appendRules };
-    expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
+    expect(praiseSinger.givePraise('John')).toBe('John rules!');
 
     praiseSinger.givePraise = appendDoubleRules;
-    expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
-
+    expect(praiseSinger.givePraise('Mary')).toBe('Mary totally rules!');
   });
 });
