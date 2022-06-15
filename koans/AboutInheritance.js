f6b897a7fa8d9dde4eb4a118d18a93a5d67112a0
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-06-15 17:27:16 +0900

    commit_test

--- koans/AboutInheritance.js
+++ koans/AboutInheritance.js
@@ -29,7 +29,7 @@ describe('About inheritance', function () {
   });
 
   it('should be able to call a method on the base object', function () {
-    expect(this.swedishChef.answerNanny()).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.answerNanny()).toEqual("Everything's cool!");
   });
 
   it('should set constructor parameters on the base object', function () {
