d35f6b7f0615324e3a6fdbc1b77cbbfac7cdbe3f
Djess-V <e.ageevets@yandex.ru>
2023-03-28 18:13:16 +0700

    Изменения внесены. Тесты пройдены.

--- koans/AboutInheritance.js
+++ koans/AboutInheritance.js
@@ -2,43 +2,43 @@ function Muppet(age, hobby) {
   this.age = age;
   this.hobby = hobby;
 
-  this.answerNanny = function(){
-	return "Everything's cool!";
-  }
+  this.answerNanny = function () {
+    return "Everything's cool!";
+  };
 }
 
 function SwedishChef(age, hobby, mood) {
   Muppet.call(this, age, hobby);
   this.mood = mood;
 
-  this.cook = function() {
+  this.cook = function () {
     return "Mmmm soup!";
-  }
+  };
 }
 
 SwedishChef.prototype = new Muppet();
 
-describe("About inheritance", function() {
-  beforeEach(function(){
+describe("About inheritance", function () {
+  beforeEach(function () {
     this.muppet = new Muppet(2, "coding");
-	this.swedishChef = new SwedishChef(2, "cooking", "chillin");
+    this.swedishChef = new SwedishChef(2, "cooking", "chillin");
   });
 
-  it("should be able to call a method on the derived object", function() {
-    expect(this.swedishChef.cook()).toEqual(FILL_ME_IN);
+  it("should be able to call a method on the derived object", function () {
+    expect(this.swedishChef.cook()).toEqual("Mmmm soup!");
   });
 
-  it("should be able to call a method on the base object", function() {
-    expect(this.swedishChef.answerNanny()).toEqual(FILL_ME_IN);
+  it("should be able to call a method on the base object", function () {
+    expect(this.swedishChef.answerNanny()).toEqual("Everything's cool!");
   });
 
-  it("should set constructor parameters on the base object", function() {
-    expect(this.swedishChef.age).toEqual(FILL_ME_IN);
-    expect(this.swedishChef.hobby).toEqual(FILL_ME_IN);
+  it("should set constructor parameters on the base object", function () {
+    expect(this.swedishChef.age).toEqual(2);
+    expect(this.swedishChef.hobby).toEqual("cooking");
   });
 
-  it("should set constructor parameters on the derived object", function() {
-    expect(this.swedishChef.mood).toEqual(FILL_ME_IN);
+  it("should set constructor parameters on the derived object", function () {
+    expect(this.swedishChef.mood).toEqual("chillin");
   });
 });
 
@@ -47,39 +47,39 @@ Object.prototype.beget = function () {
   function F() {}
   F.prototype = this;
   return new F();
-}
+};
 
 function Gonzo(age, hobby, trick) {
   Muppet.call(this, age, hobby);
   this.trick = trick;
 
-  this.doTrick = function() {
+  this.doTrick = function () {
     return this.trick;
-  }
+  };
 }
 
 //no longer need to call the Muppet (base type) constructor
 Gonzo.prototype = Muppet.prototype.beget();
 
-describe("About Crockford's inheritance improvement", function() {
-  beforeEach(function(){
-  this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
+describe("About Crockford's inheritance improvement", function () {
+  beforeEach(function () {
+    this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
   });
 
-  it("should be able to call a method on the derived object", function() {
-    expect(this.gonzo.doTrick()).toEqual(FILL_ME_IN);
+  it("should be able to call a method on the derived object", function () {
+    expect(this.gonzo.doTrick()).toEqual("eat a tire");
   });
 
-  it("should be able to call a method on the base object", function() {
-    expect(this.gonzo.answerNanny()).toEqual(FILL_ME_IN);
+  it("should be able to call a method on the base object", function () {
+    expect(this.gonzo.answerNanny()).toEqual("Everything's cool!");
   });
 
-  it("should set constructor parameters on the base object", function() {
-    expect(this.gonzo.age).toEqual(FILL_ME_IN);
-    expect(this.gonzo.hobby).toEqual(FILL_ME_IN);
+  it("should set constructor parameters on the base object", function () {
+    expect(this.gonzo.age).toEqual(3);
+    expect(this.gonzo.hobby).toEqual("daredevil performer");
   });
 
-  it("should set constructor parameters on the derived object", function() {
-    expect(this.gonzo.trick).toEqual(FILL_ME_IN);
+  it("should set constructor parameters on the derived object", function () {
+    expect(this.gonzo.trick).toEqual("eat a tire");
   });
 });
--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -1,51 +1,55 @@
 describe("About Objects", function () {
-
   describe("Properties", function () {
     var megalomaniac;
 
     beforeEach(function () {
-       megalomaniac = {  mastermind: "Joker", henchwoman: "Harley" };
+      megalomaniac = { mastermind: "Joker", henchwoman: "Harley" };
     });
 
     it("should confirm objects are collections of properties", function () {
-      expect(megalomaniac.mastermind).toBe(FILL_ME_IN);
+      expect(megalomaniac.mastermind).toBe("Joker");
     });
 
     it("should confirm that properties are case sensitive", function () {
-      expect(megalomaniac.henchwoman).toBe(FILL_ME_IN);
-      expect(megalomaniac.henchWoman).toBe(FILL_ME_IN);
+      expect(megalomaniac.henchwoman).toBe("Harley");
+      expect(megalomaniac.henchWoman).toBe(undefined);
     });
   });
 
-
   it("should know properties that are functions act like methods", function () {
     var megalomaniac = {
-      mastermind : "Brain",
+      mastermind: "Brain",
       henchman: "Pinky",
       battleCry: function (noOfBrains) {
-        return "They are " + this.henchman + " and the" +
-          Array(noOfBrains + 1).join(" " + this.mastermind);
-      }
+        return (
+          "They are " +
+          this.henchman +
+          " and the" +
+          Array(noOfBrains + 1).join(" " + this.mastermind)
+        );
+      },
     };
 
     var battleCry = megalomaniac.battleCry(4);
-    expect(FILL_ME_IN).toMatch(battleCry);
+    expect("They are Pinky and the Brain Brain Brain Brain Brain").toMatch(
+      battleCry
+    );
   });
 
   it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
     var currentDate = new Date();
-    var currentYear = (currentDate.getFullYear());
+    var currentYear = currentDate.getFullYear();
     var megalomaniac = {
       mastermind: "James Wood",
       henchman: "Adam West",
       birthYear: 1970,
       calculateAge: function () {
         return currentYear - this.birthYear;
-      }
+      },
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2023);
+    expect(megalomaniac.calculateAge()).toBe(53);
   });
 
   describe("'in' keyword", function () {
@@ -54,56 +58,52 @@ describe("About Objects", function () {
       megalomaniac = {
         mastermind: "The Monarch",
         henchwoman: "Dr Girlfriend",
-        theBomb: true
+        theBomb: true,
       };
     });
 
     it("should have the bomb", function () {
-
       var hasBomb = "theBomb" in megalomaniac;
 
-      expect(hasBomb).toBe(FILL_ME_IN);
+      expect(hasBomb).toBe(true);
     });
 
     it("should not have the detonator however", function () {
-
       var hasDetonator = "theDetonator" in megalomaniac;
 
-      expect(hasDetonator).toBe(FILL_ME_IN);
+      expect(hasDetonator).toBe(false);
     });
   });
 
   it("should know that properties can be added and deleted", function () {
-    var megalomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
+    var megalomaniac = { mastermind: "Agent Smith", henchman: "Agent Smith" };
 
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(false);
 
     megalomaniac.secretary = "Agent Smith";
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(true);
 
     delete megalomaniac.henchman;
-    expect("henchman" in megalomaniac).toBe(FILL_ME_IN);
+    expect("henchman" in megalomaniac).toBe(false);
   });
 
-
   it("should use prototype to add to all objects", function () {
-      function Circle(radius)
-      {
-        this.radius = radius;
-      }
+    function Circle(radius) {
+      this.radius = radius;
+    }
 
-      var simpleCircle = new Circle(10);
-      var colouredCircle = new Circle(5);
-      colouredCircle.colour = "red";
+    var simpleCircle = new Circle(10);
+    var colouredCircle = new Circle(5);
+    colouredCircle.colour = "red";
 
-      expect(simpleCircle.colour).toBe(FILL_ME_IN);
-      expect(colouredCircle.colour).toBe(FILL_ME_IN);
+    expect(simpleCircle.colour).toBe(undefined);
+    expect(colouredCircle.colour).toBe("red");
 
-      Circle.prototype.describe = function () {
-        return "This circle has a radius of: " + this.radius;
-      };
+    Circle.prototype.describe = function () {
+      return "This circle has a radius of: " + this.radius;
+    };
 
-      expect(simpleCircle.describe()).toBe(FILL_ME_IN);
-      expect(colouredCircle.describe()).toBe(FILL_ME_IN);
+    expect(simpleCircle.describe()).toBe("This circle has a radius of: 10");
+    expect(colouredCircle.describe()).toBe("This circle has a radius of: 5");
   });
 });
