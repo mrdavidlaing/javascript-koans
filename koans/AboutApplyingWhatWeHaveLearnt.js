aa766fcfffc2c3d0944e0ab1db6e281012c256d1
TatianaInzhee <tatiana@fastforward.ai>
2023-04-16 23:29:46 +0200

    Correct files

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,18 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
-      var productsICanEat = [];
+      var productsICanEat = products.filter (p =>
+        !p.containsNuts && !p.ingredients.includes("mushrooms")
+      )
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +57,20 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+    //var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    
+    var sum = _.range(1, 1000).reduce((accumulator, currentValue) => {
+        if (currentValue % 3 === 0 || currentValue % 5 === 0) { return accumulator + currentValue } 
+        else return accumulator
+      }, 0
+    );   
+    
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,7 +83,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
