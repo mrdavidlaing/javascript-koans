b942e09b72236ae404856bbfa46e9cd633963119
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-01-24 14:23:16 +0300

    Fix format of File AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -2,106 +2,106 @@ var _; //globals
 
 describe("About Applying What We Have Learnt", function() {
 
-  var products;
+    var products;
 
-  beforeEach(function () {
-    products = [
-       { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
-       { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
-       { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
-       { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
-       { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
-    ];
-  });
+    beforeEach(function () {
+        products = [
+            { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
+            { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
+            { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
+            { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
+            { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
+        ];
+    });
 
-  /*********************************************************************************/
+    /*********************************************************************************/
 
-  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+    it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
 
-    var i,j,hasMushrooms, productsICanEat = [];
+        var i,j,hasMushrooms, productsICanEat = [];
 
-    for (i = 0; i < products.length; i+=1) {
-        if (products[i].containsNuts === false) {
-            hasMushrooms = false;
-            for (j = 0; j < products[i].ingredients.length; j+=1) {
-               if (products[i].ingredients[j] === "mushrooms") {
-                  hasMushrooms = true;
-               }
+        for (i = 0; i < products.length; i+=1) {
+            if (products[i].containsNuts === false) {
+                hasMushrooms = false;
+                for (j = 0; j < products[i].ingredients.length; j+=1) {
+                    if (products[i].ingredients[j] === "mushrooms") {
+                        hasMushrooms = true;
+                    }
+                }
+                if (!hasMushrooms) productsICanEat.push(products[i]);
             }
-            if (!hasMushrooms) productsICanEat.push(products[i]);
         }
-    }
 
-    expect(productsICanEat.length).toBe(1);
-  });
+        expect(productsICanEat.length).toBe(1);
+    });
 
-  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+    it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
-      var productsICanEat = [];
+        var productsICanEat = [];
 
-      /* solve using filter() & all() / any() */
-      productsICanEat = _(products).filter(function (product) {
-          return !product.containsNuts && !_(product.ingredients).any(function (ingredient) {
-              return ingredient === "mushrooms"
-          });
-      });
+        /* solve using filter() & all() / any() */
+        productsICanEat = _(products).filter(function (product) {
+            return !product.containsNuts && !_(product.ingredients).any(function (ingredient) {
+                return ingredient === "mushrooms"
+            });
+        });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
-  });
+        expect(productsICanEat.length).toBe(FILL_ME_IN);
+    });
 
-  /*********************************************************************************/
+    /*********************************************************************************/
 
-  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
+    it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
 
-    var sum = 0;
-    for(var i=1; i<1000; i+=1) {
-      if (i % 3 === 0 || i % 5 === 0) {
-        sum += i;
-      }
-    }
-
-    expect(sum).toBe(233168);
-  });
-
-  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-
-    var sum = _(_.range(1, 1000))
-          .chain()
-          .filter(function (num) {
-              return num % 3 === 0 || num % 5 === 0;
-          })
-          .reduce(function (sum, num) {
-              return sum + num;
-          }).value(); /* try chaining range() and reduce() */
-
-    expect(233168).toBe(sum);
-  });
-
-  /*********************************************************************************/
-   it("should count the ingredient occurrence (imperative)", function () {
-    var ingredientCount = { "{ingredient name}": 0 };
-
-    for (i = 0; i < products.length; i+=1) {
-        for (j = 0; j < products[i].ingredients.length; j+=1) {
-            ingredientCount[products[i].ingredients[j]] = (ingredientCount[products[i].ingredients[j]] || 0) + 1;
+        var sum = 0;
+        for(var i=1; i<1000; i+=1) {
+            if (i % 3 === 0 || i % 5 === 0) {
+                sum += i;
+            }
         }
-    }
 
-    expect(ingredientCount['mushrooms']).toBe(2);
-  });
+        expect(sum).toBe(233168);
+    });
 
-  it("should count the ingredient occurrence (functional)", function () {
-    var ingredientCount = { "{ingredient name}": 0 };
+    it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    /* chain() together map(), flatten() and reduce() */
-      _(products).chain()
-          .map(function (product) { return product.ingredients;})
-          .flatten()
-          .reduce(function (memo, ingredient) { memo[`${ingredient}`] = (memo[`${ingredient}`] || 0) + 1; return memo}, ingredientCount)
-          .value();
+        var sum = _(_.range(1, 1000))
+            .chain()
+            .filter(function (num) {
+                return num % 3 === 0 || num % 5 === 0;
+            })
+            .reduce(function (sum, num) {
+                return sum + num;
+            }).value(); /* try chaining range() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(2);
-  });
+        expect(233168).toBe(sum);
+    });
+
+    /*********************************************************************************/
+    it("should count the ingredient occurrence (imperative)", function () {
+        var ingredientCount = { "{ingredient name}": 0 };
+
+        for (i = 0; i < products.length; i+=1) {
+            for (j = 0; j < products[i].ingredients.length; j+=1) {
+                ingredientCount[products[i].ingredients[j]] = (ingredientCount[products[i].ingredients[j]] || 0) + 1;
+            }
+        }
+
+        expect(ingredientCount['mushrooms']).toBe(2);
+    });
+
+    it("should count the ingredient occurrence (functional)", function () {
+        var ingredientCount = { "{ingredient name}": 0 };
+
+        /* chain() together map(), flatten() and reduce() */
+        _(products).chain()
+            .map(function (product) { return product.ingredients;})
+            .flatten()
+            .reduce(function (memo, ingredient) { memo[`${ingredient}`] = (memo[`${ingredient}`] || 0) + 1; return memo}, ingredientCount)
+            .value();
+
+        expect(ingredientCount['mushrooms']).toBe(2);
+    });
 
     it("should find the largest prime factor of a composite number", function () {
         function isPrime(number) {
